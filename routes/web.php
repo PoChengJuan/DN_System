fa773f755dcca9a70e206b3eb3bf1299b0914f9e
PoCheng <bigbangetw@gmail.com>
2018-04-25 22:13:15 +0800

    20180425-2

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,14 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+  echo "adfadfadsf";
+  echo '<meta http-equiv="Content-Type" content="text/html; charset=utf-8">'; //網頁編碼宣告
+  echo '這裡是字串<br>';
+  echo '<a href="">這裡是超連結</a><br>';
+  echo "string"; '<b>這裡是粗體字</b><br>';
+  echo "<input type='text' value=''><br>";
+  echo "<input type='button' value='這是按鈕'><br>";
+  //echo Form::file('image');
+  echo Form::select('size', array('L' => 'Large', 'S' => 'Small'));
+  return view('welcome');
 });
